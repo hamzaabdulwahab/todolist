621c9d75c801d7b017b8ff3455e150cb409b6cba
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2024-12-31 20:31:31 +0500

    [Bug] Fixed the issue in the ListView file, addressing the functionality of the sign in and sign up buttons.

--- ToDoListSwift/View/ListView.swift
+++ ToDoListSwift/View/ListView.swift
@@ -10,65 +10,67 @@ import TodoListModule
 
 struct ListView: View {
     @State var name: String = ""
-    @State var isShowingProfile = false
+    @State private var isShowingProfile = false
     @EnvironmentObject var listViewModel: ListViewModel
     @EnvironmentObject var viewModel: AuthViewModel
-    
+
     var body: some View {
-        if viewModel.userSession != nil {
-            VStack {
-                ZStack {
-                    if listViewModel.items.isEmpty {
-                        NoItemsView()
-                            .transition(AnyTransition.opacity.animation(.easeIn))
-                    } else {
-                        List {
-                            ForEach(listViewModel.items) { item in
-                                ListRowView(item: item)
-                                    .onTapGesture {
-                                        withAnimation(.linear) {
-                                            listViewModel.updateItem(item: item)
+        VStack {
+            Group {
+                if viewModel.userSession != nil {
+                    ZStack {
+                        if listViewModel.items.isEmpty {
+                            NoItemsView()
+                                .transition(AnyTransition.opacity.animation(.easeIn))
+                        } else {
+                            List {
+                                ForEach(listViewModel.items) { item in
+                                    ListRowView(item: item)
+                                        .onTapGesture {
+                                            withAnimation(.linear) {
+                                                listViewModel.updateItem(item: item)
+                                            }
                                         }
-                                    }
+                                }
+                                .onDelete(perform: listViewModel.deleteItem)
+                                .onMove(perform: listViewModel.moveItem)
                             }
-                            .onDelete(perform: listViewModel.deleteItem)
-                            .onMove(perform: listViewModel.moveItem)
+                            .listStyle(PlainListStyle())
                         }
-                        .listStyle(PlainListStyle())
                     }
-                }
-                .navigationTitle("Todo List 📝")
-                .navigationBarItems(
-                    leading: EditButton(),
-                    trailing: NavigationLink("Add", destination: AddView())
-                )
-                
-                Spacer()
-                HStack {
-                    Spacer()
+                    .navigationTitle("Todo List 📝")
+                    .navigationBarItems(
+                        leading: EditButton(),
+                        trailing: NavigationLink("Add", destination: AddView())
+                    )
                     
-                    Button {
-                        isShowingProfile = true
-                    } label: {
-                        Image(systemName: "gear")
-                            .font(.system(size: 28))
-                            .foregroundColor(Color(.systemGray))
-                            .padding()
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        
+                        // Profile Button - This is where the profile view is triggered
+                        Button {
+                            isShowingProfile = true
+                        } label: {
+                            Image(systemName: "gear")
+                                .font(.system(size: 28))
+                                .foregroundColor(Color(.systemGray))
+                                .padding()
+                        }
                     }
+                } else {
+                    LoginView()
                 }
             }
-            .fullScreenCover(isPresented: $isShowingProfile, content: {
-                ProfileView()
-
-            })
-        } else {
-            LoginView()
+        }
+        .fullScreenCover(isPresented: $isShowingProfile) {
+            ProfileView()
         }
     }
 }
 
 #Preview {
-    NavigationView {
+    NavigationView{
         ListView()
     }
     .environmentObject(ListViewModel())
